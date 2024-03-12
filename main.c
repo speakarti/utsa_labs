d52d68e496b6c5748d1357d3b32174c2b676fc99
agupta <agupta@hl001>
2024-03-11 19:03:17 -0500

    base_project

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/main.c to edit this template
+ */
+
+/* 
+ * File:   main.c
+ * Author: agupta
+ *
+ * Created on March 11, 2024, 6:58 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("hello");
+    return (EXIT_SUCCESS);
+}
+
